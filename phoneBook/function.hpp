3ad58eb9015780a159b5c5b99460c8d325992f74
Lyashenko Sergey <2268746@mail.ru>
2023-06-04 17:05:39 +0300

    half work

---
+++ phoneBook/function.hpp
@@ -0,0 +1,13 @@
+//
+//  function.hpp
+//  phoneBook
+//
+//  Created by Sergey Lyashenko on 04.06.2023.
+//
+
+#ifndef function_hpp
+#define function_hpp
+
+#include <stdio.h>
+
+#endif /* function_hpp */
