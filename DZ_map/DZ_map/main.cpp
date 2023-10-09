9dfb071e961530e3c0839c0649ffe6184c305862
Lyashenko Sergey <2268746@mail.ru>
2023-10-09 12:29:00 +0300

    end dz

---
+++ DZ_map/DZ_map/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DZ_map
+//
+//  Created by Sergey Lyashenko on 09.10.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
