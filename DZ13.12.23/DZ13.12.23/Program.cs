328920947f508706ed99ae2848cad6994c88488d
Lyashenko Sergey <2268746@mail.ru>
2023-12-01 12:26:47 +0300

    end

---
+++ DZ13.12.23/DZ13.12.23/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+
+namespace Logger
+{
+    public class Loger
+    {
+        private string logFilePath;
+        private string configFilePath;
+
+        public Loger(string logFilePath, string configFilePath)
+        {
+            this.logFilePath = logFilePath;
+            this.configFilePath = configFilePath;
+        }
+
+        public void LogMessage(string type, string message)
+        {
+            string logMessage = $"{DateTime.Now.ToString()} - {type} - {Environment.UserName} - {message}";
+
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.WriteLine(logMessage);
+            }
+        }
+
+        public void LoadConfig()
+        {
+            string typeMask = ConfigurationManager.AppSettings["TypeMask"];
+            string messageMask = ConfigurationManager.AppSettings["MessageMask"];
+            Console.WriteLine("Type mask: " + typeMask);
+            Console.WriteLine("Message mask: " + messageMask);
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loger loger = new Loger("log.txt", "config.ini");
+            loger.LogMessage("Error", "An error occurred in the application");
+            loger.LogMessage("Information", "User logged in successfully");
+            loger.LoadConfig();
+        }
+    }
+}
