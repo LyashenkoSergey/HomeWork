aa62fa87042cd86869183cbb9aa57528e46e2281
Lyashenko Sergey <2268746@mail.ru>
2023-12-01 12:33:44 +0300

    end

---
+++ DZ04.12.23/DZ04.12.23/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите нзвание файла для копирования");
+        string sourceFilePath = Console.ReadLine();
+        Console.WriteLine("Введите название нового файла");
+        string destinationFilePath = Console.ReadLine();
+        CopyFile(sourceFilePath, destinationFilePath);
+
+        Console.WriteLine("Файл скопирован");
+    }
+
+    static void CopyFile(string sourcePath, string destinationPath)
+    {
+        using (FileStream source = new FileStream(sourcePath, FileMode.Open))
+        {
+            using (FileStream destination = new FileStream(destinationPath, FileMode.Create))
+            {
+                byte[] buffer = new byte[4086];
+
+                int bytesRead;
+                while ((bytesRead = source.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    destination.Write(buffer, 0, bytesRead);
+                }
+            }
+        }
+    }
+}
+
